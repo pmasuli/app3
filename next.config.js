a40323059c7454c04a4c7a7ce8bc2c1448ac4058
pmasuli <pmasuli@hotmail.com>
2026-03-30 19:52:14 -0700

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    // loadPaths: Turbopack/new Sass API; includePaths: Webpack/legacy Sass API
+    loadPaths: [
+      path.join(__dirname, 'node_modules'),
+      path.join(__dirname, 'node_modules/bootstrap/scss'),
+    ],
+    includePaths: [
+      path.join(__dirname, 'node_modules'),
+      path.join(__dirname, 'node_modules/bootstrap/scss'),
+    ],
+  },
 }
 
 module.exports = nextConfig
